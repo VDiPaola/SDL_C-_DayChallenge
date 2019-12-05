fcf6dc98e7c7f489c8b61980c35880bdd00f4e0b
Enzo Di Paola <dipaolae98@gmail.com>
2019-12-05 09:32:11 +0000

    Initial Commit

---
+++ SDL_Program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_Program
+//
+//  Created by Enzo on 05/12/2019.
+//  Copyright © 2019 Marta,Anoop,Enzo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
